f8092f6682653d8fab38b5222c60cd93a61d2635
aufarizqi210 <aufarizqi210@gmail.com>
2025-04-30 23:33:13 +0700

    Apply Design System (Foundations)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styles from './Home.module.scss';
 
 export const Home: React.FC = () => {
   return (
     <div>
-      <h1 style={{ color: 'white' }}>My Portofolio</h1>
+      <h1 className={styles.tes}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        asperiores iste necessitatibus velit odio voluptate doloremque magnam
+        expedita laborum, repudiandae mollitia quisquam natus quo sequi suscipit
+        iusto! Labore, quas voluptatem!
+      </h1>
+
+      <div className={styles.box}></div>
     </div>
   );
 };
